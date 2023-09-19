d74ff89836b301db5523155ae684abb1f0882c28
David SPORN <sporniket.studio@gmail.com>
2023-09-19 09:47:50 +0200

    video source is a solid, blinking color

--- src/the_cradle/the_cradle.py
+++ src/the_cradle/the_cradle.py
@@ -32,50 +32,50 @@ from amaranth_stuff.modules import Sequencer
 
 ## pll parameters to get the frequencies [270, 108, 27, 31]MHz
 mainPllParameters = {
-    "p_PLLRST_ENA":"DISABLED",
-    "p_INTFB_WAKE":"DISABLED",
-    "p_STDBY_ENABLE":"DISABLED",
-    "p_DPHASE_SOURCE":"DISABLED",
-    "p_OUTDIVIDER_MUXA":"DIVA",
-    "p_OUTDIVIDER_MUXB":"DIVB",
-    "p_OUTDIVIDER_MUXC":"DIVC",
-    "p_OUTDIVIDER_MUXD":"DIVD",
-    "p_CLKI_DIV":5,
-    "p_CLKOP_ENABLE":"ENABLED",
-    "p_CLKOP_DIV":2,
-    "p_CLKOP_CPHASE":1,
-    "p_CLKOP_FPHASE":0,
-    "p_CLKOS_ENABLE":"ENABLED",
-    "p_CLKOS_DIV":5,
-    "p_CLKOS_CPHASE":1,
-    "p_CLKOS_FPHASE":0,
-    "p_CLKOS2_ENABLE":"ENABLED",
-    "p_CLKOS2_DIV":20,
-    "p_CLKOS2_CPHASE":1,
-    "p_CLKOS2_FPHASE":0,
-    "p_CLKOS3_ENABLE":"ENABLED",
-    "p_CLKOS3_DIV":17,
-    "p_CLKOS3_CPHASE":1,
-    "p_CLKOS3_FPHASE":0,
-    "p_FEEDBK_PATH":"CLKOP",
-    "p_CLKFB_DIV":54,
-    "i_RST":0,
-    "i_STDBY":0,
-    "i_PHASESEL0":0,
-    "i_PHASESEL1":0,
-    "i_PHASEDIR":1,
-    "i_PHASESTEP":1,
-    "i_PHASELOADREG":1,
-    "i_PLLWAKESYNC":0,
-    "i_ENCLKOP":0
+    "p_PLLRST_ENA": "DISABLED",
+    "p_INTFB_WAKE": "DISABLED",
+    "p_STDBY_ENABLE": "DISABLED",
+    "p_DPHASE_SOURCE": "DISABLED",
+    "p_OUTDIVIDER_MUXA": "DIVA",
+    "p_OUTDIVIDER_MUXB": "DIVB",
+    "p_OUTDIVIDER_MUXC": "DIVC",
+    "p_OUTDIVIDER_MUXD": "DIVD",
+    "p_CLKI_DIV": 5,
+    "p_CLKOP_ENABLE": "ENABLED",
+    "p_CLKOP_DIV": 2,
+    "p_CLKOP_CPHASE": 1,
+    "p_CLKOP_FPHASE": 0,
+    "p_CLKOS_ENABLE": "ENABLED",
+    "p_CLKOS_DIV": 5,
+    "p_CLKOS_CPHASE": 1,
+    "p_CLKOS_FPHASE": 0,
+    "p_CLKOS2_ENABLE": "ENABLED",
+    "p_CLKOS2_DIV": 20,
+    "p_CLKOS2_CPHASE": 1,
+    "p_CLKOS2_FPHASE": 0,
+    "p_CLKOS3_ENABLE": "ENABLED",
+    "p_CLKOS3_DIV": 17,
+    "p_CLKOS3_CPHASE": 1,
+    "p_CLKOS3_FPHASE": 0,
+    "p_FEEDBK_PATH": "CLKOP",
+    "p_CLKFB_DIV": 54,
+    "i_RST": 0,
+    "i_STDBY": 0,
+    "i_PHASESEL0": 0,
+    "i_PHASESEL1": 0,
+    "i_PHASEDIR": 1,
+    "i_PHASESTEP": 1,
+    "i_PHASELOADREG": 1,
+    "i_PLLWAKESYNC": 0,
+    "i_ENCLKOP": 0,
 }
 
 ## Video timings for 720x576@50Hz
 ## sequence of pixels in a single scan line [sync, back porch, active, front porch]
-pixelSequence = [64,68,720,12] # total = 864
+pixelSequence = [64, 68, 720, 12]  # total = 864
 
 ## sequence of scanlines in a video screen [sync, back porch, active, front porch]
-scanlineSequence = [5,39,576,5] # total = 625
+scanlineSequence = [5, 39, 576, 5]  # total = 625
 
 
 class TheCradle(Elaboratable):
@@ -86,57 +86,73 @@ class TheCradle(Elaboratable):
         m = Module()
 
         ## Setup blinky : shows that the fpga is running
-        m.submodules.blinky = Blinky()
+        m.submodules.blinky = blinky = Blinky()
 
         ### Main pll and clocks
         m.submodules.mainPll = mainPll = PllInstance(mainPllParameters)
 
-        domDviLink = ClockDomain("dviLink", local = True) # for the shift registers receiving TMDS encoded data
+        domDviLink = ClockDomain(
+            "dviLink", local=True
+        )  # for the shift registers receiving TMDS encoded data
         m.d.comb += domDviLink.clk.eq(mainPll.clkout0)
 
-        domTheCradle = ClockDomain("theCradle", local = True) # save for later, unused now
+        domTheCradle = ClockDomain(
+            "theCradle", local=True
+        )  # save for later, unused now
         m.d.comb += domTheCradle.clk.eq(mainPll.clkout1)
 
-        domPixel = ClockDomain("pixel", local = True) # for the TMDS encoder and the pixel source.
-        m.d.comb += domPixel.clk.eq(mainPll.clkout2, local = True)
+        domPixel = ClockDomain(
+            "pixel", local=True
+        )  # for the TMDS encoder and the pixel source.
+        m.d.comb += domPixel.clk.eq(mainPll.clkout2, local=True)
 
-        domCpuBase = ClockDomain("cpuBase", local = True) # base for clocking the external CPU, unused now
-        m.d.comb += domCpuBase.clk.eq(mainPll.clkout3, local = True)
+        domCpuBase = ClockDomain(
+            "cpuBase", local=True
+        )  # base for clocking the external CPU, unused now
+        m.d.comb += domCpuBase.clk.eq(mainPll.clkout3, local=True)
 
         ### Pixel sequencer -> hsync clock
-        m.submodules.pixelSequencer = pixelSequencer = DomainRenamer("pixel")(Sequencer(pixelSequence))
+        m.submodules.pixelSequencer = pixelSequencer = DomainRenamer("pixel")(
+            Sequencer(pixelSequence)
+        )
         hsync = pixelSequencer.steps[0]
         henable = pixelSequencer.steps[2]
 
-        domHsync = ClockDomain("hsync", local = True)
+        domHsync = ClockDomain("hsync", local=True)
         m.d.comb += domHsync.clk.eq(hsync)
 
         ### Scanline sequencer -> vsync clock + video display enable
-        m.submodules.scanlineSequencer = scanlineSequencer = DomainRenamer("hsync")(Sequencer(scanlineSequence))
+        m.submodules.scanlineSequencer = scanlineSequencer = DomainRenamer("hsync")(
+            Sequencer(scanlineSequence)
+        )
         vsync = scanlineSequencer.steps[0]
         venable = scanlineSequencer.steps[2]
 
         vde = Signal()
         m.d.comb += vde.eq(venable & henable)
 
-        hsyncActive = Signal() # Hsync pulse only for lines with displayable area
+        hsyncActive = Signal()  # Hsync pulse only for lines with displayable area
         m.d.comb += hsyncActive.eq(venable & hsync)
 
-        domVsync = ClockDomain("vsync", local = True)
+        domVsync = ClockDomain("vsync", local=True)
         m.d.comb += domVsync.clk.eq(vsync)
 
-        domHsyncActive = ClockDomain("hsyncActive", local = True) # in effect, serves to clock a counter of displayed line, reset at each vbl.
-        m.d.comb += [
-            domHsyncActive.clk.eq(hsyncActive),
-            domHsyncActive.rst.eq(vsync)
-        ]
+        domHsyncActive = ClockDomain(
+            "hsyncActive", local=True
+        )  # in effect, serves to clock a counter of displayed line, reset at each vbl.
+        m.d.comb += [domHsyncActive.clk.eq(hsyncActive), domHsyncActive.rst.eq(vsync)]
 
         ### The pixel source
-        # TODO : step 0 : solid blue-ish screen
+        # TODO : step 0 : blinking solid blue-ish/solid grey-ish screen
+        videoSolidBlink = Signal(24)  # v[0:8] = blue, v[8:16] = green, v[16:24] = blue
+        m.d.comb += videoSolidBlink.eq(
+            Mux(blinky.submodules.beat.beat_p, 0x0055AA, 0xAA9955)
+        )
+
         # TODO : step 1 : checkered, 16x8 pattern
         # TODO : step 2 : colorful gradient tiles
         ### The dvi link
-        # TODO : the ressource gpios of the hdmi link, using the output of the shift registers 
+        # TODO : the ressource gpios of the hdmi link, using the output of the shift registers
         # TODO : the shift registers, using the TMDS encoder outputs
         # TODO : the TMDS encoder, using the pixel source output
 
@@ -146,7 +162,5 @@ class TheCradle(Elaboratable):
         # * Through a display plugged into the HDMI port, a pattern (either solid blue-ish, or a checkered pattern, or colored gradient tiles)
         #
         ###
-
-
 
         return m
